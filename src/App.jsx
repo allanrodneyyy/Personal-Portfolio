7c8e1bbd657f411a5220d01a2107f6ce20763e1c
qt boi <yourqtboi@gmail.com>
2026-07-13 10:54:10 +1000

    Add portfolio animations

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,61 @@ import { Subheader } from './components/Subheader'
 import { Projects } from './components/Projects'
 import { Footer } from './components/Footer'
 import { Routes, Route } from 'react-router'
+import Particles from './components/Particles'
+import AnimatedContent from './components/AnimatedContent'
+
 
 function App() {
+
   return (
-    <>
+    <div className="relative min-h-screen ">
+
+      <div className="fixed inset-0 -z-10">
+        <Particles
+          particleColors={['#ffffff']}
+          particleCount={200}
+          particleSpread={12}
+          speed={0.08}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={true}
+        />
+      </div>
+
       <Header />
-      <Subheader />
-      <Skills />
-      <Projects />
+
+      <AnimatedContent
+        distance={100}
+        direction="vertical"
+        duration={0.8}
+      >
+        <Subheader />
+      </AnimatedContent>
+
+
+      <AnimatedContent
+        distance={100}
+        direction="vertical"
+        duration={1}
+      >
+        <Skills />
+      </AnimatedContent>
+
+
+      <AnimatedContent
+        distance={100}
+        direction="vertical"
+        duration={0.8}
+      >
+        <Projects />
+      </AnimatedContent>
+
+
       <Footer />
-    </>
+    </div>
   )
 }
+
+
 
 export default App
