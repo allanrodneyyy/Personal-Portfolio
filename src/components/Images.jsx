0c1565125d0341209dd6e75c76c3bee04e064c5d
qt boi <yourqtboi@gmail.com>
2026-07-27 18:07:11 +1000

    Updated Images and Projects

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useRef } from "react";
 import { useTheme } from '../context/ThemeContext';
-
 import studentTaskList from "../assets/screenshots/student-task-list-1.png";
+import reactStudentList from "../assets/screenshots/student-task-react.png";
 import vanta from "../assets/screenshots/vanta-1.png";
 import shop from "../assets/screenshots/jadeys-1.png";
-import { FaCss3, FaHtml5, FaJava } from "react-icons/fa";
+import { FaCss3, FaHtml5, FaJava, FaGit } from "react-icons/fa";
+import { VscCode } from 'react-icons/vsc';
 import { IoLogoJavascript } from "react-icons/io";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { GrMysql } from "react-icons/gr";
@@ -20,18 +21,31 @@ export function Images({ setProjectNumber }) {
 
   const images = [{
     id: 0,
+    title: 'Student Task List in React.js',
+    path: reactStudentList,
+    subtitle: 'Web Development',
+    videoUrl: 'www.youtube.com/watch?v=FJYt50AA5oo',
+    status: 'On going',
+    tools: [{
+      html: <FaHtml5 color="#E34F26" />,
+      css: <RiTailwindCssFill color='#22D3EE' />,
+      js: <IoLogoJavascript color='#F7DF1E' />,
+      editor: <VscCode color='#0065A9' />,
+      git: <FaGit color='#E34F26' />
+    }]
+  }, {
+    id: 1,
     title: 'Vanta',
     path: vanta,
     subtitle: 'E-Commerce Website',
-    videoUrl: 'www.youtube.com/watch?v=32M1al-Y6Ag&t',
-    status: 'In Progress',
+    status: 'Paused',
     tools: [{
       html: <FaHtml5 color="#E34F26" />,
       css: <RiTailwindCssFill color='#22D3EE' />,
       js: <IoLogoJavascript color='#F7DF1E' />
     }]
   }, {
-    id: 1,
+    id: 2,
     title: 'Student Task List',
     path: studentTaskList,
     subtitle: 'Web Development',
@@ -42,7 +56,7 @@ export function Images({ setProjectNumber }) {
       js: <IoLogoJavascript color='#F7DF1E' />
     }]
   }, {
-    id: 2,
+    id: 3,
     title: 'Point of Sales and Reservation System',
     path: shop,
     subtitle: 'C# Windows Application Form',
@@ -85,17 +99,33 @@ export function Images({ setProjectNumber }) {
     };
   }, [setProjectNumber]);
 
+  function handleClick(url) {
+
+  }
+
   return (
     <>
       {images.map((image, index) => (
         <div key={image.id} className={`border-dashed border border-gray-400 rounded-md shadow-md tracking-tighter`}>
-          <div className="hover:cursor-pointer">
-            <img className="object-fill p-5 "
-              data-index={index}
-              src={image.path}
-              ref={(el) => (imgRefs.current[index] = el)}
-            />
-          </div>
+          {image.videoUrl ?
+            <a href="https://student-task-list.vercel.app/" target="_blank" className="hover:cursor-pointer" >
+              <img className="object-fill p-5 "
+                data-index={index}
+                src={image.path}
+                ref={(el) => (imgRefs.current[index] = el)}
+              />
+            </a>
+
+            : <div className="" >
+              <img className="object-fill p-5 "
+                data-index={index}
+                src={image.path}
+                ref={(el) => (imgRefs.current[index] = el)}
+              />
+            </div>
+          }
+
+
           <div className={`flex gap-2 justify-between p-2 font-roboto-condensed font-semibold border-dashed border-gray-400 border-b border-t`}>
             <p>{image.title} - {image.subtitle}</p>
             <p>{image.status}</p>
@@ -109,6 +139,8 @@ export function Images({ setProjectNumber }) {
                 <p>{tool.html} </p>
                 <p>{tool.css}</p>
                 <p>{tool.js}</p>
+                <p>{tool.editor}</p>
+                <p>{tool.git}</p>
               </div>
             ))}
           </div>
